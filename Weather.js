efc4f0e50f72b4f8338864ae450b7ac45fce78ac
Muhammad Usman <63476835+usman52@users.noreply.github.com>
2023-04-17 23:59:54 +0500

    Add files via upload

---
+++ Weather.js
@@ -0,0 +1,113 @@
+import './Weather.css';
+import {useState, useEffect} from 'react';
+
+const api = {
+    key: "b47139f1a8ee99b6597bde6bd3634d6c",
+    base: "https://api.openweathermap.org/data/2.5/"
+};
+function Weather(){
+    const CountryList = [
+        {   id: '1',
+            name: 'Pakistan',
+            
+        },
+        {
+            id: '2',
+            name: 'Canada',
+
+        }
+    ];
+    const cityList = [
+        {
+            id: '1', countryId: '1', name: 'Islamabad'
+        },
+        {
+        id: '2', countryId: '1', name: 'Lahore'},
+        {
+            id: '3', countryId: '2', name: 'Ottawa'
+        },
+        {
+            id: '4', countryId: '2', name: 'Toronto'
+        }
+    ];
+    
+    
+    const [select, setSelect] = useState("");
+    const [weather, setWeather] = useState({});
+    const [country, setCountry] = useState([]);
+    const [city, setCity] = useState([]);
+    console.log(city);
+    
+    const selectInput = () => {
+        fetch(`${api.base}weather?q=${select}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) =>{
+            setWeather(result);
+        });
+    };
+    useEffect(()=>{
+        setCountry(CountryList);
+    },[])
+
+    const handleCountry = (id) =>{
+        const dt = cityList.filter(name =>name.countryId === id);
+        setCity(dt);
+       
+    }
+    
+
+    
+    return(
+        <>
+        <div className = "container">
+            <div><h2>To Know Temperature</h2></div>
+            <div><h4>Select Country and City</h4></div>
+            <div>
+            <select id = "country" onChange= {(e) => {handleCountry(e.target.value);
+            }}>
+            <option value = '0'>Select Country</option>
+            {
+                country && country !== undefined? country.map((ctr, index) => {
+                    return(
+                        <option key = {index} value={ctr.id} name = {ctr.name}>{ctr.name}</option>                    )
+
+                })
+                :'No Country'
+            }
+                
+                
+            </select>
+            <br></br> 
+            <select id = 'city'>
+            <option onChange={(e) => setSelect(e.target.value)}>--Select City--</option>
+            
+            {
+            city && city !== undefined? city.map((ctr, index) => {
+                    return(
+                        <option key = {index} value={ctr.id}>{ctr.name}</option>                    )
+
+                })
+                :'No City'
+            }
+                
+            </select>
+            </div>
+            <div>
+                <button onClick = {selectInput}>Check</button>
+            </div>
+            {typeof weather.main != "undefined" ?(
+            <div>
+                <p>{weather.name}</p>
+                <p>{weather.main.temp} °C</p>
+            </div>
+            ):(
+                ""
+            )}
+
+         
+        </div>
+        
+        </>
+    );
+}
+export default Weather;
